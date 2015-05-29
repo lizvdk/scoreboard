bb426436efab2fb0d51722ffa724e991b933ac15
Liz Vanderkloot <liz@barbershoplabs.com>
2015-05-29 00:42:22 -0400

    Break code into smaller functions

--- js/main.js
+++ js/main.js
@@ -18,6 +18,26 @@ $(document).ready(function() {
     return 0;
   }
 
+  function determineRanks(scores) {
+    scores[0].rank = 1;
+    if (scores.length > 1 ) {
+      for (var i = 1; i < scores.length; i++) {
+        if (scores[i].points === scores[i-1].points) {
+          scores[i].rank = scores[i-1].rank;
+        } else {
+          scores[i].rank = scores[i-1].rank + 1;
+        }
+      }
+    }
+  }
+
+  function displayScores(scores) {
+    $("#rankings").empty();
+    _(scores).forEach(function(n) {
+      $("#rankings").append("<li>" + "<b>"+ n.rank + ". " + "</b>" + n.name + ", " + n.points + " pts." + "</li>");
+    });
+  }
+
   var scores = [];
 
   var Score = function (name, points) {
@@ -39,25 +59,10 @@ $(document).ready(function() {
       scores.push(newScore);
     }
 
-    $("#rankings").empty();
     $("input").val("");
-
     scores.sort(compare);
-
-    scores[0].rank = 1;
-    if (scores.length > 1 ) {
-      for (var i = 1; i < scores.length; i++) {
-        if (scores[i].points === scores[i-1].points) {
-          scores[i].rank = scores[i-1].rank;
-        } else {
-          scores[i].rank = scores[i-1].rank + 1;
-        }
-      }
-    }
-
-    _(scores).forEach(function(n) {
-      $("#rankings").append("<li>" + "<b>"+ n.rank + ". " + "</b>" + n.name + ", " + n.points + " pts." + "</li>");
-    });
+    determineRanks(scores);
+    displayScores(scores);
   });
 
   $("#clear").click(function(){
