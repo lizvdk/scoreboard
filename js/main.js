e02a8df38db1fdc68c559a7a8464ced2db2d56eb
Liz Vanderkloot <liz@barbershoplabs.com>
2015-05-28 23:23:36 -0400

    Update player's points total if they already exist

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
 $(document).ready(function() {
+
+  function findExisting(name) {
+    var found = false;
+    for (var i = 0; i < scores.length; i++) {
+      if(scores[i].name == name) {
+        found = true;
+        return scores[i];
+      }
+    }
+  }
+
   var scores = [];
 
   var Score = function (name, points) {
@@ -11,8 +22,13 @@ $(document).ready(function() {
     var name = input[0];
     var points = parseInt(input[1]);
 
-    var newScore = new Score(name, points);
-    scores.push(newScore);
+    if (findExisting(name)) {
+      var player = findExisting(name);
+      player.points += points;
+    } else {
+      var newScore = new Score(name, points);
+      scores.push(newScore);
+    }
 
     var sorted = _(scores).sortBy("points").reverse();
 
